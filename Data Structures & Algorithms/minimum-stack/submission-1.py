62aadc29215832658650b67da35d61fec4a69d60
Gabriel Contreras <gjcm2007@gmail.com>
2026-04-29 15:36:15 -0400

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.py
@@ -0,0 +1,21 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        top = self.stack [-1]
+        return top
+
+    def getMin(self) -> int:
+        res = 100000000000000
+        for i in range(len(self.stack)): 
+            res = min(self.stack[i], res)
+        return res
