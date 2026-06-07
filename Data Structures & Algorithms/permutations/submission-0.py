f81d984f16a92fce8801afcae316086eb69a23cf
Gabriel Contreras <gjcm2007@gmail.com>
2026-06-07 19:09:16 -0400

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        self.res = []
+        self.backtrack(nums, 0)
+        return self.res
+    def backtrack(self, nums, indx): 
+        if indx == len(nums):
+            self.res.append(nums[:])
+            return
+        for i in range(indx, len(nums)):
+            nums[indx], nums[i] = nums[i], nums[indx]
+            self.backtrack(nums, indx + 1)
+            nums[indx] , nums[i] = nums[i], nums[indx]
