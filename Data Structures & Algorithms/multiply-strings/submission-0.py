e704d3620f258c302d89a68c65f37c2a753a91f7
Gabriel Contreras <gjcm2007@gmail.com>
2026-08-21 19:33:40 -0400

    Add: multiply-strings - submission-0

---
+++ Data Structures & Algorithms/multiply-strings/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        num1copy = int(num1)
+        num2copy = int(num2)
+
+        res = num1copy * num2copy
+
+        return str(res)
